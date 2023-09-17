ec76f586308bf6d310d41d36e7551add3d253c08
adityap5 <aditya29pippal@gmail.com>
2023-09-17 23:42:33 +0530

    1

---
+++ script.js
@@ -0,0 +1,8 @@
+var image =document.querySelector("img");
+var heart = document.querySelector("i");
+
+image.addEventListener("dblclick",function(){
+ heart.style.opacity ="0.4";
+ heart.style.scale=""
+
+})

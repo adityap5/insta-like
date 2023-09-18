fcb72bd57707ac3909575f4747c5b2c1ff5496ec
adityap5 <aditya29pippal@gmail.com>
2023-09-18 18:30:49 +0530

    done

--- script.js
+++ script.js
@@ -2,23 +2,20 @@ var image =document.querySelector("img");
 var heart = document.querySelector("i");
 
 image.addEventListener("dblclick",function(){
- heart.style.opacity ="0.4";
- heart.style.scale ="1.5";
- console.log("a");
- setTimeout(function(){
-    heart.style.opacity ="0";
- console.log("b");
-
- },1000)
- setTimeout(function(){
-    heart.style.opacity ="0.4";
- console.log("c");
-
- },1000)
+ heart.style.opacity ="0.6";
+ heart.style.scale ="1";
 
  setTimeout(function(){
-    heart.style.opacity ="0";
- console.log("d");
+ heart.style.opacity ="0";
+ },1000) ;
+ setTimeout(function(){
+   heart.style.scale ="0";
+   },1000) ;
 
- },1000)
+
+
+
+
 })
+ 
+

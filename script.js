471f7df9eaf6ee56bb3bbd6e23a1f37a535f1de0
adityap5 <aditya29pippal@gmail.com>
2023-09-18 18:51:32 +0530

    mousemove

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var image =document.querySelector("img");
 var heart = document.querySelector("i");
-
+var crsr = document.querySelector(".cursor")
+var body = document.querySelector("body");
 image.addEventListener("dblclick",function(){
  heart.style.opacity ="0.6";
  heart.style.scale ="1";
@@ -11,11 +12,11 @@ image.addEventListener("dblclick",function(){
  setTimeout(function(){
    heart.style.scale ="0";
    },1000) ;
-
-
-
-
-
 })
  
+body.addEventListener("mousemove",function(dets){
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+
+})
 
